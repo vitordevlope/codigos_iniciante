2afa19becaef73a2169a73ec5b6acd98ccd02e0a
vitordevlope <118639231+vitordevlope@users.noreply.github.com>
2023-05-28 10:21:21 -0300

    adicionando arquivos do projeto

---
+++ IdeaProjects/java_contador_for/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+            System.out.println("Olá, este é um programa simples em java");
+            System.out.println("Ele envia como saida na tela os números de 1 a 10,\nusando um loop for.\n");
+            for(int i = 1; i<=10; i++) {
+                System.out.println(i);
+
+            }
+        }
+    }
+
